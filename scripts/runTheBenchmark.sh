f936e064e8195e521f798f195371aee74e5bb22c
Lokesh Rangineni <lokeshforjava@gmail.com>
2022-01-12 12:56:23 -0500

    Checking in the openshift yaml files to spin up benchmark client and mongodb with different topologies. Checking in the run benchmark script file with little modifications.

--- scripts/runTheBenchmark.sh
+++ scripts/runTheBenchmark.sh
@@ -4,27 +4,31 @@
 # ./runTheBenchmark.sh mongodb-benchmark-replica-set-1  database-benchmark-quarkus-client-867485c897-kfdx2
 
 MONGO_POD_NAME=$1   # Mongo Pod Name from Open Shift Cluster
-BENCHMARK_CLIENT_POD_NAME=$2
+BENCHMARK_CLIENT_POD_NAME=$2     # benchmark client Pod Name from Open Shift Cluster
 NUMBER_OF_USERS="1 4 8 20 40 60 80 100 120 160 200"
 
 echo "Current date : $(date) @ $(hostname)"
 
 # Iterate the string variable using for loop
 for USER in $NUMBER_OF_USERS; do
-    echo "------------------------------------------"
+    echo "---------------------------------------------------------"
     printf "\n Running test for number of users - %s \n" $USER
 
     printf "Delete the existing mongo database collection \n"
     ./delete-existing-collection.sh $MONGO_POD_NAME
 
     printf "\nTriggering the warm up benchmark in %s \n" $BENCHMARK_CLIENT_POD_NAME
-    printf "curl -X GET http://localhost:9090/benchmark/databaseWrite/20/30 \n"
+    printf "curl -X GET http://localhost:9090/benchmark/databaseWrite/20/2 \n"
     # Trigger the test request
-    oc exec $BENCHMARK_CLIENT_POD_NAME -- /bin/sh -c "curl -X GET http://localhost:9090/benchmark/databaseWrite/20/30"
+    oc exec $BENCHMARK_CLIENT_POD_NAME -- /bin/sh -c "curl -X GET http://localhost:9090/benchmark/databaseWrite/20/2"
 
-     printf "\nTriggering the actual benchmark in %s \n" $BENCHMARK_CLIENT_POD_NAME
-     printf "curl -X GET http://localhost:9090/benchmark/databaseWrite/30/%s \n" $USER
-     oc exec $BENCHMARK_CLIENT_POD_NAME -- /bin/sh -c "curl -X GET http://localhost:9090/benchmark/databaseWrite/120/$USER"
+    printf "\nTriggering the actual benchmark in %s \n" $BENCHMARK_CLIENT_POD_NAME
+    printf "curl -X GET http://localhost:9090/benchmark/databaseWrite/120/%s \n" $USER
+    oc exec $BENCHMARK_CLIENT_POD_NAME -- /bin/sh -c "curl -X GET http://localhost:9090/benchmark/databaseWrite/120/$USER"
 
-    echo "------------------------------------------"
+    printf "\nTest completed for the user - %s \n" $USER
+    # Sleep for some time so that resources can reset.
+    sleep 2m
+    echo "---------------------------------------------------------"
+
 done
